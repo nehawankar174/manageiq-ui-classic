8b22e4b271661f36f4b4ad045fce80c83dde21b7
neha.wankar <nehawankar174@gmail.com>
2019-01-14 17:10:23 +0530

    change conditions

--- app/helpers/application_helper/button/instance_detach.rb
+++ app/helpers/application_helper/button/instance_detach.rb
@@ -2,7 +2,7 @@ class ApplicationHelper::Button::InstanceDetach < ApplicationHelper::Button::Bas
   def visible?
     # FIXME: feature for attach/detach is missing, testing class for now
     # Click2Cloud: Added telefonica and orange cloudmanager condition
-    @record.kind_of?(ManageIQ::Providers::Openstack::CloudManager::Vm) || @record.kind_of?(ManageIQ::Providers::Telefonica::CloudManager::Vm) || @record.kind_of?(ManageIQ::Providers::Orange::CloudManager::Vm)
+    @record.kind_of?(ManageIQ::Providers::Openstack::CloudManager::Vm || ManageIQ::Providers::Telefonica::CloudManager::Vm || ManageIQ::Providers::Orange::CloudManager::Vm)
   end
 
   def disabled?
